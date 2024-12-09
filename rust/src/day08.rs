10081d33d8648c171bb0a96fb9c1f27721850735
Mahmoud SALMI <mahmoud@salmi.dev>
2024-12-09 10:38:08 +0100

    day 08 - rust solution

---
+++ rust/src/day08.rs
@@ -0,0 +1,140 @@
+use crate::tools::read_raw_data;
+use crate::DaySolution;
+use std::collections::{HashMap, HashSet};
+use std::ops::{Add, Sub};
+
+#[derive(Clone, Copy, Hash, Eq, PartialEq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Add for Position {
+    type Output = Position;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Position::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Sub for Position {
+    type Output = Position;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Position::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+pub struct Data {
+    width: i32,
+    height: i32,
+    all: HashSet<Position>,
+    indexed: HashMap<char, HashSet<Position>>,
+}
+
+impl Data {
+    fn new(width: i32, height: i32) -> Self {
+        Self {
+            width,
+            height,
+            all: HashSet::new(),
+            indexed: HashMap::new(),
+        }
+    }
+
+    fn add_antenna(&mut self, c: char, x: i32, y: i32) {
+        let pos = Position::new(x, y);
+        self.all.insert(pos);
+        self.indexed
+            .entry(c)
+            .or_insert(HashSet::new())
+            .insert(pos);
+    }
+
+    fn in_grid(&self, pos: &Position) -> bool {
+        pos.x >= 0 && pos.x < self.width && pos.y >= 0 && pos.y < self.height
+    }
+
+    fn calculate_antinodes(&self, antenna1: Position, antenna2: Position, with_resonance: bool) -> Vec<Position> {
+        let diff = antenna2 - antenna1;
+        if diff.x == 0 && diff.y == 0 {
+            return Vec::new();
+        }
+
+        let mut antinodes = Vec::new();
+        let mut antinode: Position;
+
+        if !with_resonance {
+            antinode = antenna2 + diff;
+            if self.in_grid(&antinode) {
+                antinodes.push(antinode);
+            }
+            antinode = antenna1 - diff;
+            if self.in_grid(&antinode) {
+                antinodes.push(antinode);
+            }
+        } else {
+            antinode = antenna2.clone();
+            while self.in_grid(&antinode) {
+                antinodes.push(antinode);
+                antinode = antinode + diff;
+            }
+            antinode = antenna1.clone();
+            while self.in_grid(&antinode) {
+                antinodes.push(antinode);
+                antinode = antinode - diff;
+            }
+        }
+
+        antinodes
+    }
+
+    fn calculate_all_antinodes(&self, with_resonance: bool) -> HashSet<Position> {
+        self.indexed.values()
+            .flat_map(|antennas| {
+                antennas.iter().flat_map(|antenna1| {
+                    antennas.iter().flat_map(move |antenna2| {
+                        self.calculate_antinodes(*antenna1, *antenna2, with_resonance)
+                    })
+                })
+            })
+            .collect::<HashSet<Position>>()
+    }
+}
+
+pub struct Day08Solution;
+
+impl DaySolution<Data, usize> for Day08Solution {
+    fn day(&self) -> u16 { 8 }
+
+    fn parse_input(&self, example: bool) -> Data {
+        let raw_data = read_raw_data(self.day(), example);
+        let raw_data = raw_data.lines().collect::<Vec<&str>>();
+
+        let mut data = Data::new(raw_data[0].len() as i32, raw_data.len() as i32);
+
+        raw_data.iter().enumerate().for_each(|(y, line)| {
+            line.chars().enumerate().for_each(|(x, c)| {
+                if c != '.' {
+                    data.add_antenna(c, x as i32, y as i32);
+                }
+            });
+        });
+
+        data
+    }
+
+    fn part1(&self, input: &Data) -> usize {
+        input.calculate_all_antinodes(false).len()
+    }
+
+    fn part2(&self, input: &Data) -> usize {
+        input.calculate_all_antinodes(true).len()
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -11,6 +11,7 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
+mod day08;
 
 pub trait DaySolution<I, O> {
     fn day(&self) -> u16;
@@ -87,4 +88,5 @@ fn main() {
     get_and_store_result(Box::new(day05::Day05Solution {}), day_range.clone());
     get_and_store_result(Box::new(day06::Day06Solution {}), day_range.clone());
     get_and_store_result(Box::new(day07::Day07Solution {}), day_range.clone());
+    get_and_store_result(Box::new(day08::Day08Solution {}), day_range.clone());
 }
